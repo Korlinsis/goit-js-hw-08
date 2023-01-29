79b991d4c8ce44a7e25d7e9fdf8625d37e3deea0
Ivan Kiziuk <ivanmnksos@gmail.com>
2023-01-29 21:02:24 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,14 +1,15 @@
 import throttle from "lodash.throttle";
 
+const LOCAL_STORAGE_KEY = "feedback-form-state";
 const feedbackForm = document.querySelector(".feedback-form");
+const {elements: { email, message }} = feedbackForm;
 let formState = {email: "", message: ""};
 
 feedbackForm.addEventListener("input", throttle(handleFormInput, 500));
 feedbackForm.addEventListener("submit", handleFormSubmit);
 
-if (localStorage.getItem("feedback-form-state")){
-    formState = JSON.parse(localStorage.getItem("feedback-form-state"));
-    const {elements: { email, message }} = feedbackForm;
+if (localStorage.getItem(LOCAL_STORAGE_KEY)){
+    formState = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
     email.value = formState.email;
     message.value = formState.message;
 }
@@ -20,14 +21,19 @@ function handleFormInput (e)  {
         formState.message = e.target.value;
     }
      
-    localStorage.setItem("feedback-form-state", JSON.stringify(formState));
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(formState));
 };
 
 function handleFormSubmit (e) {
     e.preventDefault();
+   
+    if (email.value === "" || message.value === "") {
+        return alert("Заповніть обидва поля форми!");
+    }
+
     console.log(formState);
     formState.email = "";
     formState.message = "";
-    localStorage.removeItem("feedback-form-state");
+    localStorage.removeItem(LOCAL_STORAGE_KEY);
     e.currentTarget.reset();
 };
